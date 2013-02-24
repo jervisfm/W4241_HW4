8c69bc08bb9c1e96cd97d2e3cdbaed83fcfdc16f
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-02-23 20:20:35 -0500

    remove unneeded code

--- graph.py
+++ graph.py
@@ -39,44 +39,8 @@ def plot_random_points():
     plt.show()
     fig.savefig('test3.png')
 
-def func(n):
-    return 1 / np.sqrt(n)
 
-def func2(x):
-    return 1 / np.square(x)
-
-def plot_file(file):
-    f = open(file)
-
-    line = f.readline()
-    x = []
-    y = []
-    while line:
-        row = split(line, ',')
-        sample_size = long(row[0])
-        y.append(sample_size)
-
-        err = float(row[1])
-        x.append(err)
-        line = f.readline()
-
-
-    fig = figure()
-    plt.xlabel('Error')
-    plt.ylabel('No Samples')
-
-    print x
-    print y
-
-
-    plt.plot(x, y, 'r--', x,y, 'bo')
-
-    plt.show()
-    fig.savefig('graph.png')
-
-
-def test():
-    plot_file('data.csv')
-
-
-plot_random_points()
+if __name__ == '__main__':
+    print "Random Number Generator Visualizer"
+    plot_random_points()
+    print "Graph Plotted"
