fc07f7a10db1418b7421aab2cac72d5616c9e7b4
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-02-23 20:17:40 -0500

    add code to plot random points

--- graph.py
+++ graph.py
@@ -1,5 +1,6 @@
 from math import sqrt
 from string import split
+import random
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 import numpy as np
@@ -7,6 +8,36 @@ from matplotlib.mlab import stineman_interp
 
 
 __author__ = 'Jervis'
+
+# Returns a Random point in a Unit Square
+def get_random_point():
+    x = random.uniform(0,1)
+    y = random.uniform(0,1)
+    return [x, y]
+
+def get_random_points(n):
+    points = []
+    for _ in xrange(n):
+        point = get_random_point()
+        points.append(point)
+    return points
+
+def plot_random_points():
+    n  = 512
+    points = get_random_points(n)
+    x_points = []
+    y_points = []
+    for point in points:
+        x_points.append(point[0])
+        y_points.append(point[1])
+
+    fig = figure()
+    fig.suptitle('Visualizing Output of a Random Number Generator', fontsize=14, fontweight='bold')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.plot(x_points, y_points, 'ro')
+    plt.show()
+    fig.savefig('test3.png')
 
 def func(n):
     return 1 / np.sqrt(n)
@@ -33,6 +64,7 @@ def plot_file(file):
     fig = figure()
     plt.xlabel('Error')
     plt.ylabel('No Samples')
+
     print x
     print y
 
@@ -47,4 +79,4 @@ def test():
     plot_file('data.csv')
 
 
-test()
+plot_random_points()
